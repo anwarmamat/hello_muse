2f7c14fd6ea862a717c1122da705b2e24627d805
anwarmamat <anwarmamat@users.noreply.github.com>
2021-01-12 10:36:06 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 static char text[] = "Hello Muse!";
 
 int main() {
+    char *p = (char *)malloc(100); 
     char * printme = text;
     printf("%s\n",printme);
     return 0;
